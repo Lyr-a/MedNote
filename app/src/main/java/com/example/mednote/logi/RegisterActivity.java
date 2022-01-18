6c9dcf7c57d4406c0af1440450d4e28b33e49376
L\y/r-a <estevaoturbay@gmail.com>
2022-01-18 11:48:38 -0300

    data

--- app/src/main/java/com/example/mednote/logi/RegisterActivity.java
+++ app/src/main/java/com/example/mednote/logi/RegisterActivity.java
@@ -2,11 +2,13 @@ package com.example.mednote.logi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -14,9 +16,13 @@ import android.widget.Toast;
 
 import com.example.mednote.R;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Objects;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    String Dat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,6 @@ public class RegisterActivity extends AppCompatActivity {
 
         //region Views
 
-        //TextView TvData
         EditText EtCpf = findViewById(R.id.EtRegisterCpf);
         EditText EtNome = findViewById(R.id.EtRegisterName);
         //EditText EtData = findViewById(R.id.EtRegisterData);
@@ -33,6 +38,7 @@ public class RegisterActivity extends AppCompatActivity {
         EditText EtSenha2 = findViewById(R.id.EtRegisterPasswordRpt);
         EditText EtNumero = findViewById(R.id.EtRegisterNumber);
 
+        TextView TvData = findViewById(R.id.TvData);
         Button BtnRegister = findViewById(R.id.BtnRegister);
         Spinner SpnGenero = findViewById(R.id.SpnRegisterGender);
         Spinner SpnRegisterBloodType = findViewById(R.id.SpnRegisterBloodType);
@@ -50,10 +56,17 @@ public class RegisterActivity extends AppCompatActivity {
 
         //endregion
 
+        TvData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateDateDialog();
+            }
+        });
+
         BtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Cpf, Nome, Data, Senha, Senha2, Numero, Genero, Sangue;
+                String Cpf, Nome, Senha, Senha2, Numero, Genero, Sangue;
 
                 //region FILTRO
 
@@ -61,11 +74,17 @@ public class RegisterActivity extends AppCompatActivity {
                 Sangue = SpnRegisterBloodType.getSelectedItem().toString();
                 Cpf = EtCpf.getText().toString();
                 Nome = EtNome.getText().toString();
-                //Data = EtNome.getText().toString();
                 Senha = EtSenha.getText().toString();
                 Senha2 = EtSenha2.getText().toString();
                 Numero = EtNumero.getText().toString();
 
+
+                if (Dat.isEmpty()){
+
+                    Toast.makeText(RegisterActivity.this, "Insira uma data de aniversário clicando no botão", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
 
                 if (Genero.equals("Genero…")) {
 
@@ -104,6 +123,34 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+    }
+
+    public void CreateDateDialog () {
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        month++;
+        TextView TvData = findViewById(R.id.TvData);
+
+        if (month<10){
+            String Mon = "0" + month;
+            Dat = dayOfMonth + "/" + Mon + "/" + year;
+        }
+        else {
+            Dat = dayOfMonth + "/" + month + "/" + year;
+        }
+        TvData.setText(Dat);
 
     }
 }
